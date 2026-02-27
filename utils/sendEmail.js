bf8b377d468a57d36c617f49101e6d5b46ddeeea
shiv9568 <your@email.com>
2026-02-27 16:29:17 +0530

    feat: implement email sending utility using Nodema

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,13 +1,20 @@
 const nodemailer = require('nodemailer');
+const dns = require('dns');
+
+// Force Node.js to prefer IPv4 over IPv6. 
+// This is the most reliable fix for 'ENETUNREACH' IPv6 errors on Render.
+if (dns.setDefaultResultOrder) {
+    dns.setDefaultResultOrder('ipv4first');
+}
 
 const sendEmail = async ({ to, subject, html }) => {
     // Credentials are hardcoded below for compatibility
 
     const transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
-        port: 587,
+        port: 465,
         family: 4,
-        secure: false,
+        secure: true, // Port 465 uses SSL/TLS directly
         auth: {
             user: "shivanshbhatia9568@gmail.com",
             pass: "fzofiafctfzutedu"
@@ -15,10 +22,9 @@ const sendEmail = async ({ to, subject, html }) => {
         tls: {
             rejectUnauthorized: false
         },
-        connectionTimeout: 30000, // 30 seconds
-        greetingTimeout: 30000,
+        connectionTimeout: 45000, // 45 seconds
+        greetingTimeout: 45000,
         socketTimeout: 60000,
-        dnsTimeout: 10000,
     });
 
     const mailOptions = {
